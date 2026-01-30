948240f521fdeb4fca8b61136d8f9dffb9255dee
afletch32 <afletcherr32@gmail.com>
2026-01-30 17:20:51 -0600

    Add CORS and content-type for R2 assets

--- workers/assets/src/index.js
+++ workers/assets/src/index.js
@@ -14,8 +14,26 @@ export default {
     }
     const headers = new Headers();
     object.writeHttpMetadata(headers);
+    if (!headers.get("content-type")) {
+      const ext = key.split(".").pop().toLowerCase();
+      const fallbackTypes = {
+        ttf: "font/ttf",
+        otf: "font/otf",
+        woff: "font/woff",
+        woff2: "font/woff2",
+        png: "image/png",
+        jpg: "image/jpeg",
+        jpeg: "image/jpeg",
+        webp: "image/webp",
+        svg: "image/svg+xml",
+      };
+      if (fallbackTypes[ext]) {
+        headers.set("content-type", fallbackTypes[ext]);
+      }
+    }
     headers.set("etag", object.httpEtag);
     headers.set("cache-control", "public, max-age=31536000, immutable");
+    headers.set("access-control-allow-origin", "*");
     return new Response(object.body, { headers });
   },
 };
